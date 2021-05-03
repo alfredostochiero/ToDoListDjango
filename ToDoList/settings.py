78683e80ba7cc648d1fe399ac778af4f81829219
unknown <alfredo_stochiero@hotmail.com>
2021-05-03 17:08:23 -0300

    configuração para deploy no heroku

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+import django_heroku
 
 from pathlib import Path
 
@@ -125,3 +127,5 @@ LOGIN_URL = '/login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
